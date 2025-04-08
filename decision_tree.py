548bccafa64c2cd19451ec9a2de3cbb85b518335
Tiago <tiago.am94@gmail.com>
2025-04-08 19:26:50 -0300

    Adicionando grid search + cross validation + pipeline

--- decision_tree.py
+++ decision_tree.py
@@ -7,6 +7,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import KFold
+from sklearn.pipeline import Pipeline
 from sklearn import preprocessing
 
 
@@ -29,20 +32,35 @@ def get_train_test() -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataF
     return train_test_split(X, Y, shuffle=True, test_size=0.1, random_state=42)
 
 
-def get_classifier() -> DecisionTreeClassifier:
-    return DecisionTreeClassifier()
+def get_pipe_model() -> Pipeline:
+    return Pipeline(steps=[
+        ('normalizer', preprocessing.Normalizer()),
+        ('tree', DecisionTreeClassifier())]
+    )
+
+
+def get_grid_classifier() -> GridSearchCV:
+    # Tunando hiper parâmetros com busca de grid
+    pipe_model = get_pipe_model()
+    parameters = {'tree__max_depth': [3, 4, 5], 'tree__criterion': ['gini', 'entropy']}
+    # Validação cruzada com 5 folds
+    cv = KFold(n_splits=5, shuffle=True, random_state=42)
+    grid = GridSearchCV(pipe_model, param_grid=parameters,
+                        cv=cv, n_jobs=-1, verbose=1)
+    return grid
 
 
 def main():
     # Split do dataset
     X_train, X_test, y_train, y_test = get_train_test()
     # Instancia do classificador
-    classifier = get_classifier()
+    classifier = get_grid_classifier()
     # Treinamento
     classifier.fit(X_train, y_train)
     # Validaçao de acurácia
     score = accuracy_score(y_test, classifier.predict(X_test))
-    print(score)
+    print("Accuracy: ", score)
+    print('Best estimator: ', classifier.best_estimator_)
 
 
 if __name__ == '__main__':
