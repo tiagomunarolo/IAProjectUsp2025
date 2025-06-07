136d4a93b1220a9d26cb8276382824f7ebc725a5
Tiago <tiago.am94@gmail.com>
2025-06-07 16:44:30 -0300

    Removing outliers for better graph view

--- src/plot/plot.py
+++ src/plot/plot.py
@@ -6,6 +6,10 @@ data_dir = Path(__file__).parent.parent.joinpath('data').resolve()
 
 
 def plot_histogram(x: pd.Series, title: str):
+    if len(x.unique()) > 100:
+        # Remove outliers
+        # Remove a margem de 2.5% e 97.5%, facilitando a visualização
+        x = x[(x >= x.quantile(0.025)) & (x <= x.quantile(0.975))]
     bins = min(100, len(x.unique()))
     plt = sns.displot(x, bins=bins, kde=True)
     plt.set(title=title)
